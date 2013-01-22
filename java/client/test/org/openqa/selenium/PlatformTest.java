4a3bbd7f0b1b5fff71642fd7cbf3cf8c4eb64773
Daniel Bengtsson <danielb@opera.com>
2013-01-22 11:12:33 +0100

    fixup! Detect Windows 8 in Platform

--- java/client/test/org/openqa/selenium/PlatformTest.java
+++ java/client/test/org/openqa/selenium/PlatformTest.java
@@ -19,6 +19,7 @@ package org.openqa.selenium;
 
 import org.junit.Test;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
@@ -51,8 +52,8 @@ public class PlatformTest {
 
   @Test
   public void testWindows8Detection() {
-    assertTrue("Windows NT with os version 6.2 should be detected as Windows 8",
-               Platform.WIN8 == Platform.extractFromSysProperty("windows nt (unknown)", "6.2"));
+    assertEquals("Windows NT with os version 6.2 should be detected as Windows 8",
+               Platform.WIN8, Platform.extractFromSysProperty("windows nt (unknown)", "6.2"));
   }
 
   @Test
